e5838bef82ca5206f39f1f33b62861afdaba7486
alexgharibi <ali52346@gmail.com>
2022-01-12 14:26:10 -0500

    add product delete

--- models/product.js
+++ models/product.js
@@ -46,6 +46,16 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const updatedProducts = products.filter((p) => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
