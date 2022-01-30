29f2ed0c4820c522a432b38b33f9c088d4f0c327
alexgharibi <ali52346@gmail.com>
2022-01-30 09:21:45 -0500

    Fetch related product

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,12 +14,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
     .then((result) => res.redirect("/admin/products"))
     .catch((err) => console.log(err));
 };
@@ -30,7 +31,8 @@ exports.getEditProduct = (req, res, next) => {
     res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findAll({ where: { id: prodId } })
+  req.user
+    .getProducts({ where: { id: prodId } })
     .then((product) => {
       if (!product[0]) {
         return res.redirect("/");
@@ -67,7 +69,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
